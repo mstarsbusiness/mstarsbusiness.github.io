4489f75345158dec512a5b94e696deef5b9dce82
mstarsbusiness <mstars.business@gmail.com>
2026-08-08 12:39:29 +0800

    借址頁信任說明+KYC配套

--- build-journal.js
+++ build-journal.js
@@ -183,7 +183,7 @@ ${header('../')}
 </main>
 ${footer('../')}
 ${mobileContactBar('../')}
-<script src="../i18n.js?v=20260807"></script>
+<script src="../i18n.js?v=20260808"></script>
 </body>
 </html>`;
 }
@@ -274,7 +274,7 @@ ${header('')}
 </main>
 ${footer('')}
 ${mobileContactBar('')}
-<script src="i18n.js?v=20260807"></script>
+<script src="i18n.js?v=20260808"></script>
 </body>
 </html>`;
 }
